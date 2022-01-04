b3eeeb40b590a2d56d8a50a86e0ab99dc84429e6
boodscode237 <65740750+boodscode237@users.noreply.github.com>
2022-01-04 16:11:53 +0500

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> importantYears = new Dictionary<int, string>();
+
+            importantYears[1492] = "Columbus discovered the new world.";
+            importantYears[1969] = "Men walks on the moon.";
+            importantYears[1999] = "Something happened";
+            importantYears[2019] = "Corona virus";
+            importantYears[2021] = "Variant Omicron";
+
+            Console.WriteLine($"In the year 2019, this happened: {importantYears[2019]}");
+
+            Console.ReadLine();
+        }
+    }
+}
